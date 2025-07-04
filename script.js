6ba81174eb23d9f234fbb05b8d3dcfc6e7a37aa2
JwalithaKumar <jwalitha.techtrainer@gmail.com>
2025-07-04 13:34:44 +0530

    Added deadline

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function addTask() {
-  const taskInput = document.getElementById('taskInput');
+  const taskInput = document.getElementById("taskInput");
+  const deadlineInput = document.getElementById("deadlineInput");
+  const taskList = document.getElementById("taskList");
+
   const taskText = taskInput.value.trim();
+  const deadlineText = deadlineInput.value;
 
-  if (taskText !== '') {
-    const li = document.createElement('li');
-    li.textContent = taskText;
-
-    li.onclick = function () {
-      li.style.textDecoration = "line-through";
-    }
-
-    document.getElementById('taskList').appendChild(li);
-    taskInput.value = '';
-  } else {
-    alert('Please enter a task');
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
   }
+
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span>${taskText}</span>
+    <span class="deadline">⏰ ${deadlineText ? deadlineText : "No deadline"}</span>
+  `;
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+  deadlineInput.value = "";
 }
